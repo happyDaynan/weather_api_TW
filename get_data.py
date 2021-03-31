b5b5c7db69417735286b517d68cd74e1a926b1bc
DingNan <57606685+happyDaynan@users.noreply.github.com>
2021-03-31 16:25:00 +0800

    add get_data.py

---
+++ get_data.py
@@ -0,0 +1,32 @@
+import requests, json
+import pandas as pd
+
+
+api_requests = requests.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=Yourkey&format=JSON&elementName=TEMP&parameterName%EF%BC%8C=CITY')
+
+# str to dict
+json_data = json.loads(r.text)
+
+# 取出所要的資料
+data_list = json_data['records']['location']
+
+# 整理成所要的格式
+weather_info_list = []
+for data in data_list:
+    weather_info = {
+        "locationName" : data['locationName'],
+        "lat" : data['lat'],
+        "lon" : data['lon'],
+        "stationId" : data['stationId'],
+        "time" : data['time']['obsTime'],
+        "temp" : data['weatherElement'][0]['elementValue']
+    }
+    weather_info_list.append(weather_info)
+
+# 透過pandas 存成.csv
+df = pd.DataFrame(weather_info_list)
+df.to_csv('./data.csv', ',', encoding='utf-8', index= False)
+    
+    
+
+
